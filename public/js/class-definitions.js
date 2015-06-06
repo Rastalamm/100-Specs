571804d4c9e7c491e32de70fca3cc98d5e282b98
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-05 14:30:51 -1000

    completion of Step 65

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1146,8 +1146,8 @@ var civic = new Vehicle('Honda', 'Civic');
 var forte = new Vehicle('KIA', 'Forte');
 
 // Create 2 shapes with different numbers of sides
-var square = new Shape(4);;
-var hexagon;
+var square = new Shape(4);
+var hexagon= new Shape(6);
 
 // Create 2 boxes
 var catBox;
